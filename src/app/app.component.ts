443a10459ce547742da8e5db2f912ce24e60225f
crackm <tanuja.mandala@caerusskillmatic.com>
2018-08-24 12:17:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+
+  animations: [
+
+    trigger('listAnimation', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+          
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+
+  ]
 })
+
 export class AppComponent {
-  title = 'app';
+  items = [];
+
+  constructor() {
+    this.items = ['Hey this is Tanuja', 'Platform Developer','This is awesome'];
+  }
+
+  pushItem() {
+    this.items.push('Oh yeah that is awesome Tanuja Mandala');
+  }
+  removeItem() {
+    this.items.pop();
+  }
 }
